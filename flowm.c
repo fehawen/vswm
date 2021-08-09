ff7e758a143d32e1778725cc3763742697ec9c7c
Felix Hägglund Wennergren <felixhow@icloud.com>
2021-08-09 17:47:37 +0200

    flowm: Open and close display connection

--- flowm.c
+++ flowm.c
@@ -1,12 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/Xlib.h>
+
+static void open_display_connection(void);
+static void close_display_connection(void);
+
+static Display *display;
+
+void open_display_connection(void)
+{
+	display = XOpenDisplay(NULL);
+
+	if (!display) {
+		puts("flowm: Failed to open display connection.");
+		exit(EXIT_FAILURE);
+	}
+
+	puts("flowm: Opened display connection.");
+}
+
+void close_display_connection(void)
+{
+	XCloseDisplay(display);
+
+	puts("flowm: Closed display connection.");
+}
+
 int main(void)
 {
-	/**
-	 * Open display connection
-	 * Grab input
-	 * Handle input
-	 * Loop events
-	 * Handle events
-	 * Close display connection
-	 */
-	return 0;
+	open_display_connection();
+
+	/* Grab input */
+
+	/* Handle input */
+
+	/* Loop events */
+
+	/* Handle events */
+
+	close_display_connection();
+
+	return EXIT_SUCCESS;
 }
