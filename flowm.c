7bc73fd820f4b22fa34d3fb82d0211a0590ad4fb
Felix Hägglund Wennergren <felixhow@icloud.com>
2021-08-10 12:32:18 +0200

    flowm: Grab a single mouse button input

--- flowm.c
+++ flowm.c
@@ -2,11 +2,57 @@
 #include <stdlib.h>
 #include <X11/Xlib.h>
 
-static void loop_events(void);
 static void open_display_connection(void);
 static void close_display_connection(void);
 
+static void handle_button_press(XEvent *event);
+static void handle_button_release(XEvent *event);
+static void handle_motion_notify(XEvent *event);
+
+static void grab_input(void);
+static void loop_events(void);
+
 static Display *display;
+
+static void (*event_handlers[LASTEvent])(XEvent *event) = {
+	[ButtonPress] = handle_button_press, /* ButtonPressMask */
+	[ButtonRelease] = handle_button_release, /* ButtonReleaseMask */
+	[MotionNotify] = handle_motion_notify /* PointerMotionMask */
+};
+
+void handle_button_press(XEvent *event)
+{
+	(void)event;
+	puts("flowm: ButtonPress");
+}
+
+void handle_button_release(XEvent *event)
+{
+	(void)event;
+	puts("flowm: ButtonRelease");
+}
+
+void handle_motion_notify(XEvent *event)
+{
+	(void)event;
+	puts("flowm: MotionNotify");
+}
+
+void grab_input(void)
+{
+	XGrabButton(
+		display, /* display */
+		1, /* button */
+		Mod4Mask, /* modifiers */
+		DefaultRootWindow(display), /* grab window */
+		True, /* owner events */
+		ButtonPressMask|ButtonReleaseMask|PointerMotionMask, /* event mask */
+		GrabModeAsync, /* pointer mode */
+		GrabModeAsync, /* keyboard mode */
+		None, /* confine to */
+		None /* cursor */
+	);
+}
 
 void loop_events(void)
 {
@@ -15,7 +61,8 @@ void loop_events(void)
 	while (1) {
 		XNextEvent(display, &event);
 
-		/* Handle events */
+		if (event_handlers[event.type])
+			event_handlers[event.type](&event);
 	}
 }
 
@@ -42,7 +89,7 @@ int main(void)
 {
 	open_display_connection();
 
-	/* Grab input */
+	grab_input();
 
 	/* Handle input */
 
