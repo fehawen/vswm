dbb32c1abf4cd86d625a2c2b6d9ed43848ede0c4
Felix Hägglund Wennergren <felixhow@icloud.com>
2021-08-11 16:39:42 +0200

    flowm: Implement basic key binding functionality

--- flowm.c
+++ flowm.c
@@ -1,24 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <X11/keysym.h>
+#include <X11/XKBlib.h>
 #include <X11/Xlib.h>
 
-static void open_display_connection(void);
+#define CTRL ControlMask
+#define MOD Mod4Mask
+#define SHIFT ShiftMask
+
+typedef struct {
+	const char *argv;
+} Arg;
+
+typedef struct {
+	unsigned int modifiers;
+	KeySym symbol;
+	void (*function)(const Arg arg);
+	const Arg arg;
+} KeyBinding;
+
+typedef void (*EventHandler)(XEvent *e);
+
 static void close_display_connection(void);
-
-static void handle_button_press(XEvent *event);
-static void handle_button_release(XEvent *event);
-static void handle_motion_notify(XEvent *event);
-
+static void dummy_command(const Arg arg);
 static void grab_input(void);
+static void handle_button_press(XEvent *event);
+static void handle_key_press(XEvent *event);
 static void loop_events(void);
+static void open_display_connection(void);
 
 static Display *display;
 
-static void (*event_handlers[LASTEvent])(XEvent *event) = {
-	[ButtonPress] = handle_button_press, /* ButtonPressMask */
-	[ButtonRelease] = handle_button_release, /* ButtonReleaseMask */
-	[MotionNotify] = handle_motion_notify /* PointerMotionMask */
+static KeyBinding key_bindings[] = {
+	{ MOD | SHIFT, XK_1, dummy_command, { "foo" } },
+	{ MOD | SHIFT, XK_2, dummy_command, { "bar" } },
+	{ MOD | SHIFT, XK_3, dummy_command, { "baz" } },
 };
+
+static const EventHandler event_handler[LASTEvent] = {
+	[ButtonPress] = handle_button_press,
+	[KeyPress] = handle_key_press,
+};
+
+void close_display_connection(void)
+{
+	XCloseDisplay(display);
+
+	puts("flowm: Closed display connection.");
+}
+
+void dummy_command(const Arg arg)
+{
+	printf("flowm: %s\n", &arg.argv[0]);
+}
+
+void open_display_connection(void)
+{
+	display = XOpenDisplay(NULL);
+
+	if (!display) {
+		puts("flowm: Failed to open display connection.");
+		exit(EXIT_FAILURE);
+	}
+
+	puts("flowm: Opened display connection.");
+}
+
+void grab_input(void)
+{
+	unsigned int i;
+
+	for (i = 0; i < sizeof(key_bindings)/sizeof(*key_bindings); i++)
+		XGrabKey(
+			display,
+			XKeysymToKeycode(display, key_bindings[i].symbol),
+			key_bindings[i].modifiers,
+			DefaultRootWindow(display),
+			True,
+			GrabModeAsync,
+			GrabModeAsync
+		);
+
+	XGrabButton(
+		display,
+		1,
+		MOD,
+		DefaultRootWindow(display),
+		True,
+		ButtonPressMask,
+		GrabModeAsync,
+		GrabModeAsync,
+		None,
+		None
+	);
+}
 
 void handle_button_press(XEvent *event)
 {
@@ -35,32 +111,19 @@ void handle_button_press(XEvent *event)
 	);
 }
 
-void handle_button_release(XEvent *event)
+void handle_key_press(XEvent *event)
 {
-	(void)event;
-	puts("flowm: ButtonRelease");
-}
+	KeySym symbol;
+	unsigned int i;
 
-void handle_motion_notify(XEvent *event)
-{
-	(void)event;
-	puts("flowm: MotionNotify");
-}
+	symbol = XkbKeycodeToKeysym(display, event->xkey.keycode, 0, 0);
 
-void grab_input(void)
-{
-	XGrabButton(
-		display, /* display */
-		1, /* button */
-		Mod4Mask, /* modifiers */
-		DefaultRootWindow(display), /* grab window */
-		True, /* owner events */
-		ButtonPressMask|ButtonReleaseMask|PointerMotionMask, /* event mask */
-		GrabModeAsync, /* pointer mode */
-		GrabModeAsync, /* keyboard mode */
-		None, /* confine to */
-		None /* cursor */
-	);
+	if (!symbol)
+		return;
+
+	for (i = 0; i < sizeof(key_bindings)/sizeof(*key_bindings); i++)
+		if (symbol == key_bindings[i].symbol)
+			key_bindings[i].function(key_bindings[i].arg);
 }
 
 void loop_events(void)
@@ -70,40 +133,16 @@ void loop_events(void)
 	while (1) {
 		XNextEvent(display, &event);
 
-		if (event_handlers[event.type])
-			event_handlers[event.type](&event);
+		if (event_handler[event.type])
+			event_handler[event.type](&event);
 	}
-}
-
-void open_display_connection(void)
-{
-	display = XOpenDisplay(NULL);
-
-	if (!display) {
-		puts("flowm: Failed to open display connection.");
-		exit(EXIT_FAILURE);
-	}
-
-	puts("flowm: Opened display connection.");
-}
-
-void close_display_connection(void)
-{
-	XCloseDisplay(display);
-
-	puts("flowm: Closed display connection.");
 }
 
 int main(void)
 {
 	open_display_connection();
-
 	grab_input();
-
-	/* Handle input */
-
 	loop_events();
-
 	close_display_connection();
 
 	return EXIT_SUCCESS;
